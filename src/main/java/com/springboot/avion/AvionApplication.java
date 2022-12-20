6c2f2b32a4839e3a95591399b66b6be38e332fc7
MichaelRandria <92731876+MichaelRandria@users.noreply.github.com>
2022-12-20 16:39:03 +0300

    Add files via upload

--- src/main/java/com/springboot/avion/AvionApplication.java
+++ src/main/java/com/springboot/avion/AvionApplication.java
@@ -1,13 +1,28 @@
 package com.springboot.avion;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AvionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AvionApplication.class, args);
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/*").allowedOriginPatterns("*").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+            }
+        };
     }
 
 }
